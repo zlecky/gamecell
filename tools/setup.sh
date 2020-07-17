542fa0f4b6e2586ea79111bb8c54f3546dd1a974
lecky <yongle0624@126.com>
2020-07-17 18:34:41 +0800

    rename 3party and docs

--- tools/setup.sh
+++ tools/setup.sh
@@ -5,8 +5,8 @@ git submodule update
 
 # google-benchmark编译不过需要手动处理
 
-#1.thirdparty/gbenchmark/test/cxx03_test.cc:8:2: error: #error C++11 or greater detected. Should be C++03.
+#1.3party/gbenchmark/test/cxx03_test.cc:8:2: error: #error C++11 or greater detected. Should be C++03.
 #  #error C++11 or greater detected. Should be C++03.
 
-#2.thirdparty/gbenchmark/test/cxx03_test.cc:12:2: error: #error C++11 or greater detected by the library. BENCHMARK_HAS_CXX11 is defined.
+#2.3party/gbenchmark/test/cxx03_test.cc:12:2: error: #error C++11 or greater detected by the library. BENCHMARK_HAS_CXX11 is defined.
 #  #error C++11 or greater detected by the library. BENCHMARK_HAS_CXX11 is defined.
