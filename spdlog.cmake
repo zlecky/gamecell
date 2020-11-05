233ec124a8ecb1df376d3e48270f13409b209960
zlecky <yongle0624@126.com>
2020-11-05 18:33:29 +0800

    Convert googletest from submodule to fetchcontent.

--- spdlog.cmake
+++
@@ -1,9 +0,0 @@
-include(spdlog)
-
-fetchcontent_declare(spdlog
-    GIT_REPOSITORY https://github.com/gabime/spdlog.git
-    GIT_TAG v1.8.1
-    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3party/spdlog
-    )
-
-fetchcontent_makeavailable(spdlog)
